e939510e820bb20a3bc53e46925d7b3ce7f45ee5
Sebastian Balay <snbalay@gmail.com>
2016-03-06 11:16:20 -0300

    change identation

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    hot: true,
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
+  entry: "./entry.js",
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  hot: true,
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  }
 };
