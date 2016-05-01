4eb4ba9e27ba4b6a3c3596f1bfc8f68f64d65328
Sebastian Balay <snbalay@gmail.com>
2016-05-01 20:42:09 -0300

    initial header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function Header () {
   return (
-    <div>EL HEADER</div>
+    <header className="wb-header">
+      <img className='wb-header-logo wb-header-icon' alt=""/>
+      <img className='wb-notifications-icon wb-header-icon' alt=""/>
+      <img className='wb-suggest-icon wb-header-icon' alt=""/>
+      <img className='wb-profile-avatar wb-header-icon' alt=""/>
+    </header>
   );
 }
