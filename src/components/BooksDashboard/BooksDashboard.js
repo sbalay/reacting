dd1b9ac62d580066f505e4742c70b48437803fa9
Sebastian Balay <snbalay@gmail.com>
2016-05-01 23:54:42 -0300

    dashboard styling wip

--- src/components/BooksDashboard/BooksDashboard.js
+++ src/components/BooksDashboard/BooksDashboard.js
@@ -11,39 +11,46 @@ export default class BooksDashboard extends Component {
       books: [
         {
           author: 'qweqwe123123zxczx',
+          id: '1',
           imageUrl: 'http://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg',
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '11',
           imageUrl: `https://images.contentful.com/256tjdsmm689/2T0QeKcvR6MSsckuKoYIwS/
                      b57d12107fc5eb635e294ed1c76cbbac/feature-gettyimages.jpg`,
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '111',
           imageUrl: 'http://sabiaunite.com/uploads/gallery/12112013-080739AM-2.jpg',
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '1111',
           imageUrl: 'https://www.planwallpaper.com/static/images/magic-of-blue-universe-images.jpg',
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '11111',
           imageUrl: `http://static.ddmcdn.com/gif/storymaker-best-hubble-space
                      telescope-images-20092-514x268.jpg`,
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '111111',
           imageUrl: `http://i.dailymail.co.uk/i/pix/2015/06/26/08/
                      29F51C6A00000578-3139358-image-m-7_1435303227646.jpg`,
           title: 'asd123'
         },
         {
           author: 'qweqwe123123zxczx',
+          id: '1111111',
           imageUrl: 'https://www.planwallpaper.com/static/images/Frozen-Logo-Symbol-HD-Images.jpg',
           title: 'asd123'
         }
@@ -53,13 +60,15 @@ export default class BooksDashboard extends Component {
 
   render() {
     return (
-      <div>
+      <div className='wb-books-dashboard'>
         <BooksDashboardFilters />
-        <div className="wb-books-container">
+        <ul className="wb-books-container">
           {
-            this.state.books.map((bookParams) => <BookDetail book={ bookParams }/>)
+            this.state.books.map((bookParams) => {
+              return <li key={ bookParams.id }><BookDetail book={ bookParams }/></li>;
+            })
           }
-        </div>
+        </ul>
       </div>
     );
   }
--- src/components/BooksDashboardFilters/BooksDashboardFilters.js
+++ src/components/BooksDashboardFilters/BooksDashboardFilters.js
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
 
-export default function BooksDashboardFilters () {
-  return (
-    <div>FILTERS</div>
-  );
+export default class BooksDashboardFilters extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterTypes: ['title', 'author', 'editorial', 'category'],
+      selectedFilterType: 'title',
+      filterValue: 'asd'
+    };
+  }
+
+  onSubmitFilters () {
+    console.log('submit');
+  }
+
+  onFilterTypeSelected (selectedFilterType) {
+    this.setState({
+      selectedFilterType
+    })
+  }
+
+  onUpdateFilter (ev) {
+    this.setState({
+      filterValue: ev.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form className='wb-dasboard-filters-container' onSubmit={ this.onSubmitFilters }>
+        <DropdownButton title={ this.state.selectedFilterType }
+          id='wb-filter-type-dropdown'
+          className='wb-control-item wb-filter-type-select'>
+
+          {
+            this.state.filterTypes.map((filterType) => {
+              return (
+                <MenuItem eventKey={ filterType }
+                  key={ filterType }
+                  onSelect={ this.onFilterTypeSelected.bind(this) } >
+                  { filterType }
+                </MenuItem>
+              );
+            })
+          }
+        </DropdownButton>
+        <input type="text"
+          className="wb-input wb-control-item"
+          value={ this.state.filterValue }
+          placeholder='Buscar...'
+          onChange={ this.onUpdateFilter.bind(this) } />
+      </form>
+    );
+  }
 }
